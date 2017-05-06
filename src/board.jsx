919cacda50df86b842e81e0b983ab624a505fb8c
agatan <agatan039@gmail.com>
2017-05-06 17:54:06 +0900

    Calculate winner

--- src/board.jsx
+++ src/board.jsx
@@ -23,7 +23,7 @@ export class Board extends Component<void, Props, State> {
 
   handleClickSquare(i: number) {
     const squares = this.state.squares.slice();
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -40,7 +40,13 @@ export class Board extends Component<void, Props, State> {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (!winner) {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    } else {
+      status = 'Winner: ' + winner;
+    }
 
     return (
       <div>
@@ -63,4 +69,24 @@ export class Board extends Component<void, Props, State> {
       </div>
     );
   }
+}
+
+function calculateWinner(squares: Array<string | null>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
