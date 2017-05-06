e51db376f946fbd9d2c2bd996569d31c79e45b63
agatan <agatan039@gmail.com>
2017-05-06 13:52:16 +0900

    Enable CSS

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Hello} from './hello';
 
+import './index.css';
+
 ReactDOM.render(<Hello content="hello world"/>, document.getElementById("app"));
